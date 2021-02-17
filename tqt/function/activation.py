f02625c7f55a98fde2e28dd729172f66556461f0
Pannenets.F <you@example.com>
2021-02-17 22:15:58 +0800

    feat: add relu/relu6 support

--- tqt/function/activation.py
+++ tqt/function/activation.py
@@ -1,3 +1,51 @@
 """
 Provide quantilized form of torch.nn.modules.activation
 """
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from number import qsigned, qunsigned
+
+
+class ReLU(nn.ReLU):
+    def __init__(self, inplace=False, acti_bit_width=8, retrain=True):
+        super().__init__(inplace)
+        self.acti_bit_width = acti_bit_width
+        if retrain is True:
+            self.acti_log2_t = nn.Parameter(torch.Tensor(1))
+            self.init_param(retrain)
+        else:
+            self.acti_log2_t = torch.Tensor(1)
+            self.init_param(retrain)
+
+    def init_param(self, retrain):
+        pass
+
+    def relu_forward(self, input):
+        return qunsigned(F.relu(input), self.acti_log2_t, self.acti_bit_width)
+
+    def forward(self, input):
+        return self.relu_forward(input)
+
+
+class ReLU6(nn.ReLU6):
+    def __init__(self, inplace=False, acti_bit_width=8, retrain=True):
+        super().__init__(inplace)
+        self.acti_bit_width = acti_bit_width
+        if retrain is True:
+            self.acti_log2_t = nn.Parameter(torch.Tensor(1))
+            self.init_param(retrain)
+        else:
+            self.acti_log2_t = torch.Tensor(1)
+            self.init_param(retrain)
+
+    def init_param(self, retrain):
+        pass
+
+    def relu6_forward(self, input):
+        return qunsigned(F.relu6(input), self.acti_log2_t, self.acti_bit_width)
+
+    def forward(self, input):
+        return self.relu6_forward(input)
