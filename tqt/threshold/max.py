d8eecb34bab4f9ffc8c9d3f742a52d1851a82ba8
Pannenets.F <you@example.com>
2021-02-20 20:43:25 +0800

    fix: retrain mode should be able to disable

--- tqt/threshold/max.py
+++ tqt/threshold/max.py
@@ -4,14 +4,17 @@ import torch.nn as nn
 
 def threshold_weight_max(module):
     max_value = module.weight.abs().flatten().max().data
-    module.weight_log2_t = torch.nn.Parameter(torch.log2(max_value))
+    module.weight_log2_t = torch.nn.Parameter(
+        torch.log2(max_value)) if module.retrain else torch.log2(max_value)
 
 
 def threshold_bias_max(module):
     max_value = module.bias.abs().flatten().max().data
-    module.bias_log2_t = torch.nn.Parameter(torch.log2(max_value))
+    module.bias_log2_t = torch.nn.Parameter(
+        torch.log2(max_value)) if module.retrain else torch.log2(max_value)
 
 
 def threshold_activation_max(module):
     max_value = module.hook_out.abs().flatten().max().data
-    module.acti_log2_t = torch.nn.Parameter(torch.log2(max_value))
+    module.acti_log2_t = torch.nn.Parameter(
+        torch.log2(max_value)) if module.retrain else torch.log2(max_value)
--- tqt/threshold/sd.py
+++ tqt/threshold/sd.py
@@ -6,18 +6,24 @@ def threshold_weight_3sd(module):
     mean_value = module.weight.mean().abs().data
     std_value = module.weight.std().data
     module.weight_log2_t = torch.nn.Parameter(
-        torch.log2(mean_value + 3 * std_value))
+        torch.log2(mean_value + 3 *
+                   std_value)) if module.retrain else torch.log2(mean_value +
+                                                                 3 * std_value)
 
 
 def threshold_bias_3sd(module):
     mean_value = module.bias.mean().abs().data
     std_value = module.bias.std().data
     module.bias_log2_t = torch.nn.Parameter(
-        torch.log2(mean_value + 3 * std_value))
+        torch.log2(mean_value + 3 *
+                   std_value)) if module.retrain else torch.log2(mean_value +
+                                                                 3 * std_value)
 
 
 def threshold_activation_3sd(module):
     mean_value = module.hook_out.mean().abs().data
     std_value = module.hook_out.std().data
     module.acti_log2_t = torch.nn.Parameter(
-        torch.log2(mean_value + 3 * std_value))
+        torch.log2(mean_value + 3 *
+                   std_value)) if module.retrain else torch.log2(mean_value +
+                                                                 3 * std_value)
