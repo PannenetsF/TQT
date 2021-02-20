4cc42054d72d2c69b764a4879d77904312762e70
Pannenets.F <you@example.com>
2021-02-20 20:56:10 +0800

    fix: fix lenet example lacking of threshold

--- lenet.py
+++ lenet.py
@@ -9,17 +9,21 @@ class lenet(nn.Module):
         self.proc = nn.ModuleList()
         dic = tqt.wrapper.OFUNCTION if quant is False else tqt.wrapper.QFUNCTION
         self.proc.append(dic['Conv2d'](1, 6, 5, 1, 2))
+        self.proc.append(dic['ReLU']())
         self.proc.append(dic['MaxPool2d']((2, 2)))
         self.proc.append(dic['Conv2d'](6, 16, 5))
+        self.proc.append(dic['ReLU']())
         self.proc.append(dic['MaxPool2d']((2, 2)))
         self.proc.append(dic['Conv2d'](16, 120, 5))
+        self.proc.append(dic['ReLU']())
         self.proc.append(dic['Linear'](120, 84))
+        self.proc.append(dic['ReLU']())
         self.proc.append(dic['Linear'](84, 10))
 
     def forward(self, x):
         for idx, p in enumerate(self.proc):
             x = p(x)
-            if (idx == 4):
+            if (idx == 7):
                 x = x.reshape(x.shape[0], -1)
         return x
 
@@ -37,4 +41,5 @@ for idx, (np, qp) in enumerate(zip(net.proc, qnet.proc)):
         tqt.threshold.max.threshold_weight_max(np, qp)
     if hasattr(qp, 'bias_log2_t'):
         tqt.threshold.max.threshold_bias_max(np, qp)
+    print(idx, ': quanted')
 tqt.threshold.remove_hook(handles)
