560e471a1606e23ac339b48038d7ac06b33e487e
Pannenets.F <you@example.com>
2021-02-24 18:48:08 +0800

    feat: hook nn.{Seq, Mod-Dict, Module} supported

--- tqt/threshold/hook.py
+++ tqt/threshold/hook.py
@@ -1,3 +1,6 @@
+import torch.nn as nn
+
+
 def hook_handler(model, input, output):
     model.hook_out = output
 
@@ -12,3 +15,18 @@ def add_hook(modulelist, hook_handler):
 def remove_hook(handles):
     for handle in handles:
         handle.remove()
+
+
+def add_hook_general(layer, hook_handler):
+    handles = []
+    if isinstance(layer, nn.ModuleList):
+        handles.extend(add_hook(layer, hook_handler))
+    elif isinstance(layer, nn.Sequential):
+        for child in layer.children():
+            handles.extend(add_hook_general(child, hook_handler))
+    elif isinstance(layer, nn.ModuleDict):
+        for key in layer.keys():
+            handles.extend(add_hook_general(layer[key], hook_handler))
+    else:  # nn.Module()
+        handles.append(layer.register_forward_hook(hook_handler))
+    return handles
