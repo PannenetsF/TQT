eac8c2ac5d4c54852053ba3626b8525380c66e7f
Pannenets.F <you@example.com>
2021-02-25 08:42:12 +0800

    feat: add param, init and utils support

--- tqt/function/__init__.py
+++ tqt/function/__init__.py
@@ -1,7 +1,12 @@
 from torch.nn import Module, ModuleList, ModuleDict, Sequential
 
+from torch.nn import Parameter
+
+from . import init
+from . import utils
+
 from torch.nn import MaxPool1d, MaxPool2d, MaxPool3d
-from torch.nn import AvgPool1d, AvgPool2d, AvgPool3d 
+from torch.nn import AvgPool1d, AvgPool2d, AvgPool3d
 
 from .activation import ReLU, ReLU6
 from .batchnorm import BatchNorm2d
