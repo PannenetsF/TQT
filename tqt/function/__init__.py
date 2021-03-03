1fefafda5ab6971e55f38a2a4c99477a5ca9c14a
Pannenets.F <you@example.com>
2021-03-03 08:12:56 +0800

    fix: import error

--- tqt/function/__init__.py
+++ tqt/function/__init__.py
@@ -2,8 +2,8 @@ from torch.nn import Module, ModuleList, ModuleDict, Sequential
 
 from torch.nn import Parameter
 
-from . import init
-from . import utils
+# from torch import init
+# from torch import utils
 
 from torch.nn import MaxPool1d, MaxPool2d, MaxPool3d
 from torch.nn import AvgPool1d, AvgPool2d, AvgPool3d
