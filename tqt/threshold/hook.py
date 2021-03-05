b70012c58b160cccafb56f03721ae848eb8aa8d3
Pannenets.F <you@example.com>
2021-03-05 10:30:43 +0800

    feat: add new ways to add hook

--- tqt/threshold/hook.py
+++ tqt/threshold/hook.py
@@ -27,6 +27,38 @@ def add_hook_general(layer, hook_handler):
     elif isinstance(layer, nn.ModuleDict):
         for key in layer.keys():
             handles.extend(add_hook_general(layer[key], hook_handler))
+    elif hasattr(layer, 'proc'):
+        for key in layer.proc:
+            handles.extend(add_hook_general(getattr(layer, key), hook_handler))
     else:  # nn.Module()
         handles.append(layer.register_forward_hook(hook_handler))
     return handles
+
+
+num_list = [str(i) for i in range(0, 10)]
+
+
+def _getattr(obj, key):
+    stage = key.rsplit('.')
+    for i in stage:
+        if i[0] in num_list:
+            obj = obj[int(i)]
+        else:
+            obj = getattr(obj, i)
+    return obj
+
+
+def add_hook_fromkeys(net, hook_handler):
+    okeys = list(net.state_dict().keys())
+    mkeys = [x[:x.rfind('.')] for x in okeys]
+    handles = []
+    for key in mkeys:
+        net_proc = _getattr(net, key)
+        handles.append(net_proc.register_forward_hook(hook_handler))
+    return handles
+
+
+def add_hook_fromchild(net, hook_handler):
+    handles = []
+    for i in net.modules():
+        handles.extend(add_hook_general(i, hook_handler))
