6abd4d823167d18d156986d616fc7b492493b375
Pannenets.F <you@example.com>
2021-03-08 22:20:01 +0800

    perf: update all hook-related functions with _modules

--- tqt/threshold/__init__.py
+++ tqt/threshold/__init__.py
@@ -1,5 +1,5 @@
 from .sd import threshold_weight_3sd, threshold_bias_3sd, threshold_activation_3sd
 from .kl import entropy_calibration
 from .max import threshold_activation_max, threshold_bias_max, threshold_weight_max
-from .hook import hook_handler, add_hook, remove_hook, add_hook_general, add_hook_fromkeys, add_hook_fromchild, get_hook
+from .hook import hook_handler, remove_hook, remove_hookout, add_hook, get_hook
 from .init import init_network, init_network_fromkeys
--- tqt/threshold/hook.py
+++ tqt/threshold/hook.py
@@ -5,88 +5,50 @@ def hook_handler(model, input, output):
     model.hook_out = output
 
 
-def add_hook(modulelist, hook_handler):
-    handles = []
-    for module in modulelist:
-        handles.append(module.register_forward_hook(hook_handler))
-    return handles
-
-
 def remove_hook(handles):
     for handle in handles:
         handle.remove()
 
 
-def add_hook_general(layer, hook_handler):
-    handles = []
-    if isinstance(layer, nn.ModuleList):
-        handles.extend(add_hook(layer, hook_handler))
-    elif isinstance(layer, nn.Sequential):
-        for child in layer.children():
-            handles.extend(add_hook_general(child, hook_handler))
-    elif isinstance(layer, nn.ModuleDict):
-        for key in layer.keys():
-            handles.extend(add_hook_general(layer[key], hook_handler))
-    elif hasattr(layer, 'proc'):
-        for key in layer.proc:
-            handles.extend(add_hook_general(getattr(layer, key), hook_handler))
-    else:  # nn.Module()
-        handles.append(layer.register_forward_hook(hook_handler))
-    return handles
-
-
-num_list = [str(i) for i in range(0, 10)]
-
-
-def _getattr(obj, key):
-    stage = key.rsplit('.')
-    for i in stage:
-        if i[0] in num_list:
-            obj = obj[int(i)]
-        else:
-            obj = getattr(obj, i)
-    return obj
-
-
-def add_hook_fromkeys(net, hook_handler):
-    okeys = list(net.state_dict().keys())
-    mkeys = [x[:x.rfind('.')] for x in okeys]
-    handles = []
-    for key in mkeys:
-        net_proc = _getattr(net, key)
-        handles.append(net_proc.register_forward_hook(hook_handler))
-    return handles
-
-
-def add_hook_fromchild(net, hook_handler):
-    handles = []
-    for i in net.modules():
-        handles.extend(add_hook_general(i, hook_handler))
-
-
-def dirty_name(proc):
-    return str(type(proc)).rsplit('.')[-1][:-2]
-
-
-def get_hook(qnet_proc, name, show=False):
-    hooks_got = []
-    name += '.' + dirty_name(qnet_proc)
-    if isinstance(qnet_proc, nn.ModuleList):
-        for idx, proc in enumerate(qnet_proc):
-            hooks_got.extend(get_hook(proc, name + '.' + str(idx), show=show))
-    elif isinstance(qnet_proc, nn.Sequential):
-        for idx, proc in enumerate(qnet_proc.children()):
-            hooks_got.extend(get_hook(proc, name + '.' + str(idx), show=show))
-    elif isinstance(qnet_proc, nn.ModuleDict):
-        for key in qnet_proc.keys():
-            hooks_got.extend(
-                get_hook(qnet_proc[key], name + '.' + key, show=show))
-    elif hasattr(qnet_proc, 'proc'):
-        for key in qnet_proc.proc:
-            hooks_got.extend(
-                get_hook(getattr(qnet_proc, key), name + '.' + key, show=show))
-    else:  # nn.Module()
-        hooks_got.append((name, getattr(qnet_proc, 'hook_out')))
+def remove_hookout(layer, name, show=False):
+    keys = list(layer._modules.keys())
+    if keys == []:
         if show:
-            print(name, ' hook got')
+            print(name, 'has removed hookout')
+        delattr(layer, 'hook_out')
+    else:
+        for key in keys:
+            remove_hookout(layer._modules[key], name + '.' + key, show=show)
+
+
+def add_hook(layer, name, hook_handler, show=False):
+    handles = []
+    keys = list(layer._modules.keys())
+    if keys == []:
+        if show:
+            print(name, 'has added a hook')
+        handles.append(layer.register_forward_hook(hook_handler))
+        return handles
+    else:
+        for key in keys:
+            handles.extend(
+                add_hook(layer._modules[key],
+                         name + '.' + key,
+                         hook_handler,
+                         show=show))
+    return handles
+
+
+def get_hook(layer, name, show=False):
+    hooks_got = []
+    keys = list(layer._modules.keys())
+    if keys == []:
+        if show:
+            print(name, 'hook has been fetched')
+        hooks_got.append((name, layer.hook_out))
+        return hooks_got
+    else:
+        for key in keys:
+            hooks_got.extend(
+                get_hook(layer._modules[key], name + '.' + key, show=show))
     return hooks_got
