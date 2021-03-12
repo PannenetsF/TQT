d4e8974c15f52e29df8ee3deec81c201f414fb84
Pannenets.F <you@example.com>
2021-03-12 21:09:31 +0800

    feat: pubulish in pypi

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+# get key package details from tqt/__version__.py
+about = {}  # type: ignore
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'tqt', '__version__.py')) as f:
+    exec(f.read(), about)
+
+# load the README file and use it as the long_description for PyPI
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+# package configuration - for reference see:
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#id9
+setup(name=about['__title__'],
+      description=about['__description__'],
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      version=about['__version__'],
+      author=about['__author__'],
+      author_email=about['__author_email__'],
+      url=about['__url__'],
+      packages=['tqt'],
+      include_package_data=True,
+      python_requires=">=3.7.*",
+      install_requires=['torch'],
+      license=about['__license__'],
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: GPU',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+      keywords='quantization NN torch')
---
+++ tqt/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'tqt'
+__description__ = 'torch implement of TQT'
+__version__ = '0.1.0'
+__author__ = 'Pannenets F'
+__author_email__ = 'pannenets.f@foxmail.com'
+__license__ = 'BSD-3-Clause License'
+__url__ = 'https://github.com/PannenetsF/TQT'
