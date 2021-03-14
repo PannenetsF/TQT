f9203e8d3a09d4f86ec03973871574306ab705f6
Pannenets.F <you@example.com>
2021-03-14 16:45:05 +0800

    feat: new version

--- tqt/__version__.py
+++ tqt/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'tqt'
 __description__ = 'torch implement of TQT'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Pannenets F'
 __author_email__ = 'pannenets.f@foxmail.com'
 __license__ = 'BSD-3-Clause License'
