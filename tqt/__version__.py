fd52b37a8bb38b52f430221a6b5daefcdb95a184
pan <pan>
2021-04-02 10:13:01 +0800

    feat: new version of lite

--- tqt/__version__.py
+++ tqt/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'tqt'
 __description__ = 'torch implement of TQT'
-__version__ = '0.2.0lite'
+__version__ = '0.2.0l'
 __author__ = 'Pannenets F'
 __author_email__ = 'pannenets.f@foxmail.com'
 __license__ = 'BSD-3-Clause License'
