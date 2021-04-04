f4892b561ff3d6fb183e85f047bc6ab582fe49b3
pan <pan>
2021-04-04 23:54:14 +0800

    feat: new version

--- tqt/__version__.py
+++ tqt/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'tqt'
 __description__ = 'torch implement of TQT'
-__version__ = '0.2.0l'
+__version__ = '0.4.0'
 __author__ = 'Pannenets F'
 __author_email__ = 'pannenets.f@foxmail.com'
 __license__ = 'BSD-3-Clause License'
