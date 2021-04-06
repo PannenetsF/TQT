23f82c7c18f91592bcf4f619d28f8380012badb1
pan <pan>
2021-04-06 17:17:57 +0800

    feat: add function.convbn

--- tqt/function/__init__.py
+++ tqt/function/__init__.py
@@ -12,6 +12,7 @@ from .activation import ReLU, ReLU6
 from .batchnorm import BatchNorm2d
 from .conv import Conv2d
 from .linear import Linear
+from .convbn import Conv2dBN
 
 from . import extra
 from .adder import Adder2d
