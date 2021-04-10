22e1afd1f6c2f8203a3db255889a39164f174ca1
Pannenets.F <you@example.com>
2021-04-10 09:10:37 +0800

    feat: remove the convbn, prepare for the fold

--- tqt/function/__init__.py
+++ tqt/function/__init__.py
@@ -12,7 +12,6 @@ from .activation import ReLU, ReLU6
 from .batchnorm import BatchNorm2d
 from .conv import Conv2d
 from .linear import Linear
-from .convbn import Conv2dBN
 
 from . import extra
 from .adder import Adder2d
