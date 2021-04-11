d578376c70e5f8778763b701d22a996b5db6d8d2
Pannenets.F <you@example.com>
2021-04-11 10:22:05 +0800

    fix: self

--- tqt/fold/convbnact.py
+++ tqt/fold/convbnact.py
@@ -13,37 +13,30 @@ class Conv2dBNReLU(_FoldModule):
             self.conv = conv
             self.bn = bn
             self.relu = nn.ReLU() if isinstance(relu, nn.ReLU) else nn.ReLU6()
-            if self.bn_freezing:
-                bn_var = self.bn.running_var.detach().clone().data.reshape(
-                    -1, 1, 1, 1)
-                bn_mean = self.bn.running_mean.detach().clone().data.reshape(
-                    -1, 1, 1, 1)
-                bn_weight = self.bn.weight.detach().clone().data.reshape(
-                    -1, 1, 1, 1)
-                bn_bias = self.bn.bias.detach().clone().data.reshape(
-                    -1, 1, 1, 1)
-            else:
-                bn_var = self.bn.running_var.reshape(-1, 1, 1, 1)
-                bn_mean = self.bn.running_mean.reshape(-1, 1, 1, 1)
-                bn_weight = self.bn.weight.reshape(-1, 1, 1, 1)
-                bn_bias = self.bn.bias.reshape(-1, 1, 1, 1)
-            if self.conv.bias is not None:
-                conv_bias = self.conv.bias.reshape(-1, 1, 1, 1)
+            bn_var = bn.running_var.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_mean = bn.running_mean.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_weight = bn.weight.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_bias = bn.bias.detach().clone().data.reshape(-1, 1, 1, 1)
+            if conv.bias is not None:
+                conv_bias = conv.bias.reshape(-1, 1, 1, 1)
             else:
                 conv_bias = 0.
 
-            conv_weight = self.conv.weight * bn_weight / (bn_var +
-                                                          self.bn.eps).sqrt()
+            conv_weight = conv.weight * bn_weight / (bn_var +
+                                                          bn.eps).sqrt()
             conv_bias = bn_weight * (conv_bias - bn_mean) / (
-                bn_var + self.bn.eps).sqrt() + bn_bias
-            self.weight_bit_width = self.conv.weight_bit_width
+                bn_var + bn.eps).sqrt() + bn_bias
+            self.weight_bit_width = conv.weight_bit_width
             self.weight_log2_t = torch.nn.Parameter(
                 conv_weight.abs().max().detach().data.log2())
-            self.bias_bit_width = self.bn.bias_bit_width
+            self.bias_bit_width = bn.bias_bit_width
             self.bias_log2_t = torch.nn.Parameter(
                 conv_bias.abs().max().detach().data.log2())
-            self.acti_bit_width = self.relu.acti_bit_width
-            self.acti_log2_t = self.relu.acti_log2_t
+            self.acti_bit_width = relu.acti_bit_width
+            self.acti_log2_t = relu.acti_log2_t
         else:
             raise Exception('The folded function does not meet type check')
         self.bn_freezing = False
@@ -63,11 +56,19 @@ class Conv2dBNReLU(_FoldModule):
         self.bias_log2_t.requires_grad = False
 
     def forward(self, input):
-        bn_var = self.bn.running_var.detach().clone().data.reshape(-1, 1, 1, 1)
-        bn_mean = self.bn.running_mean.detach().clone().data.reshape(
-            -1, 1, 1, 1)
-        bn_weight = self.bn.weight.detach().clone().data.reshape(-1, 1, 1, 1)
-        bn_bias = self.bn.bias.detach().clone().data.reshape(-1, 1, 1, 1)
+        if self.bn_freezing:
+            bn_var = self.bn.running_var.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_mean = self.bn.running_mean.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_weight = self.bn.weight.detach().clone().data.reshape(
+                -1, 1, 1, 1)
+            bn_bias = self.bn.bias.detach().clone().data.reshape(-1, 1, 1, 1)
+        else:
+            bn_var = self.bn.running_var.reshape(-1, 1, 1, 1)
+            bn_mean = self.bn.running_mean.reshape(-1, 1, 1, 1)
+            bn_weight = self.bn.weight.reshape(-1, 1, 1, 1)
+            bn_bias = self.bn.bias.reshape(-1, 1, 1, 1)
         if self.conv.bias is not None:
             conv_bias = self.conv.bias.reshape(-1, 1, 1, 1)
         else:
