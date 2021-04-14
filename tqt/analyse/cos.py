625106f12609bb87d14e576ca0d3b611d6780ef0
Pannenets.F <you@example.com>
2021-04-14 16:49:56 +0800

    feat: anaylse of cos

--- tqt/analyse/cos.py
+++ tqt/analyse/cos.py
@@ -1,17 +1,25 @@
 import torch
 from ..function import qsigned, qunsigned
+from collections import OrderedDict
 
 
-def cosine_similarity(net, qnet, show=False):
+def cosine_similarity(net, qnet, wba=[8, 16, 8], show=False):
     ret = []
-    n_param, q_param = dict(net.named_parameters()), dict(
+    n_param, q_param = OrderedDict(net.named_parameters()), OrderedDict(
         qnet.named_parameters())
-    for (name, param), (_, qparam) in zip(n_param, q_param):
+    for name in n_param.keys():
         if name.find('log') == -1:
-            qparam = qsigned(qparam, q_param[name + '_log2_t'],
-                             q_param[name + '_bit_width'])
+            qparam = q_param[name]
+            if name.find('weight') != -1:
+                width = wba[0]
+            elif name.find('bias'):
+                width = wba[1]
+            else:
+                width = wba[2]
+            param = n_param[name]
+            qparam = qsigned(qparam, q_param[name + '_log2_t'], width)
             cos = param.flatten().dot(
                 qparam.flatten()) / param.norm() / qparam.norm()
             ret.append((name, cos))
-        if show:
-            print(f'layer {name} get cosine of {cos}')
+            if show:
+                print(f'layer {name} get cosine of {cos}')
--- tqt/analyse/kl.py
+++ tqt/analyse/kl.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from ..function import qsigned, qunsigned
 from ..threshold import add_hook, hook_handler, get_hook
 
@@ -10,18 +11,23 @@ def kl_divergence(ha, hb):
     return (ha * (ha / hb).log()).sum()
 
 
-def kl_similarity(net, qnet, cali_batch, show=False):
+def kl_similarity(
+        net,
+        qnet,
+        cali_batch,
+        end_list=[nn.Conv2d, nn.Linear, nn.ReLU, nn.BatchNorm2d, nn.ReLU6],
+        show=True):
     ret = []
-    add_hook(net, 'net', hook_handler, show=False)
-    add_hook(qnet, 'net', hook_handler, show=False)
+    add_hook(net, 'net', hook_handler, end_list=end_list, show=True)
+    add_hook(qnet, 'net', hook_handler, end_list=end_list, show=True)
     net(cali_batch)
     qnet(cali_batch)
-    n_hook = get_hook(net, '', show=False)
-    q_hook = get_hook(qnet, '', show=False)
+    n_hook = get_hook(net, '', show=True)
+    q_hook = get_hook(qnet, '', show=True)
+    print(n_hook)
     for n, q in zip(n_hook, q_hook):
         kl = kl_divergence(n[1].flatten(), q[1].flatten())
         ret.append((n[0], kl))
         if show:
             print(f'kl at {n[0]} is {kl}, average is {kl/n[1].numel()}')
-
     return ret
