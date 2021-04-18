33eed5101898bda86bff4ad3517310caec2dceed
Pannenets.F <you@example.com>
2021-04-18 10:15:12 +0800

    fix: quant conv require grad

--- tqt/function/conv.py
+++ tqt/function/conv.py
@@ -58,15 +58,15 @@ class Conv2d(nn.Conv2d):
 
     def quantilize(self):
         self.quant = True
-        self.weight_log2_t.requires_grad = False
-        if self.bias is not None:
-            self.bias_log2_t.requires_grad = False
-
-    def floatilize(self):
-        self.quant = False
         self.weight_log2_t.requires_grad = True
         if self.bias is not None:
             self.bias_log2_t.requires_grad = True
+
+    def floatilize(self):
+        self.quant = False
+        self.weight_log2_t.requires_grad = False
+        if self.bias is not None:
+            self.bias_log2_t.requires_grad = False
 
     def quant_answer(self):
         self.weight.data = qsigned(
