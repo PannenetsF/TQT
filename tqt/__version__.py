f0df2ea8e9b2ae59ba21dfbd511d16d212d5bfb6
Pannenets.F <you@example.com>
2021-04-18 12:24:45 +0800

    feat: stabel version

--- tqt/__version__.py
+++ tqt/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'tqt'
 __description__ = 'torch implement of TQT'
-__version__ = '0.6.0'
+__version__ = '1.0.0'
 __author__ = 'Pannenets F'
 __author_email__ = 'pannenets.f@foxmail.com'
 __license__ = 'BSD-3-Clause License'
