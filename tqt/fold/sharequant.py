9987b35cdfb2ffc913421c194553e0a169982802
Pannenets.F <you@example.com>
2021-04-18 10:52:36 +0800

    feat: add static to sharequant

--- tqt/fold/sharequant.py
+++ tqt/fold/sharequant.py
@@ -4,14 +4,20 @@ from ..function import qsigned
 
 
 class ShareQuant(nn.Module):
-    def __init__(self, acti_bit_width=8, quant=False):
+    def __init__(self, acti_bit_width=8, retrain=True, quant=False):
         super().__init__()
         self.acti_bit_width = acti_bit_width
         self.acti_log2_t = nn.Parameter(torch.Tensor(1))
+        self.retrain = retrain
         self.quant = quant
 
     def share(self, shared_module):
-        self.acti_log2_t.data = shared_module.acti_log2_t.data
+        self.acti_log2_t.data = max(shared_module.acti_log2_t.data,
+                                    self.acti_log2_t.data)
+
+    def static(self):
+        self.retrain = False
+        self.acti_log2_t.requires_grad_(False)
 
     def quantilize(self):
         self.quant = True
