df34dbf5b2e593e76a4cc77777980db7c26a5a41
pan <pan>
2021-04-30 19:58:01 +0800

    feat: show configuration status

--- tqt/config.py
+++ tqt/config.py
@@ -13,17 +13,23 @@ def config_bn(proc, config, bn_list):
     if _isinstance(proc, bn_list):
         proc.weight_bit_width = config.bn_weight
         proc.bias_bit_width = config.bn_bias
+        return True
+    return False
 
 
 def config_acti(proc, config, acti_list):
     if _isinstance(proc, acti_list):
         proc.acti_bit_width = config.acti
+        return True
+    return False
 
 
 def config_conv_linear(proc, config, conv_linear_list):
     if _isinstance(proc, conv_linear_list):
         proc.weight_bit_width = config.conv_and_linear_weight
         proc.bias_bit_width = config.conv_and_linear_bias
+        return True
+    return False
 
 
 def config_network(
@@ -35,18 +41,18 @@ def config_network(
         conv_linear_list=[nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.Linear],
         show=False):
     proc_list = list(net._modules.keys())
-    if proc_list == []:
-        if show:
-            print(name, 'is configured')
-        config_acti(net, config, acti_list=acti_list)
-        config_conv_linear(net, config, conv_linear_list=conv_linear_list)
-        config_bn(net, config, bn_list=bn_list)
-    else:
-        for n in proc_list:
-            config_network(net._modules[n],
-                           name + '.' + n,
-                           config,
-                           bn_list=bn_list,
-                           acti_list=acti_list,
-                           conv_linear_list=conv_linear_list,
-                           show=show)
+    if show and config_acti(net, config, acti_list=acti_list):
+        print(name, ' acti is configured')
+    if show and config_conv_linear(
+            net, config, conv_linear_list=conv_linear_list):
+        print(name, 'conv/linear is configured')
+    if show and config_bn(net, config, bn_list=bn_list):
+        print(name, 'bn is configured')
+    for n in proc_list:
+        config_network(net._modules[n],
+                       name + '.' + n,
+                       config,
+                       bn_list=bn_list,
+                       acti_list=acti_list,
+                       conv_linear_list=conv_linear_list,
+                       show=show)
