164414bc42c9fa11bc28bef8a8950e92f88af33b
pan <pan>
2021-04-30 20:12:27 +0800

    feat: add fold the shiftaddernet

--- tqt/fold/fold_op.py
+++ tqt/fold/fold_op.py
@@ -37,8 +37,7 @@ def fold_SAB(sa, bn):
     bn = nn.Identity()
     return sa, bn
 
-
-def fold_the_network(net):
+def fold_the_convnet(net):
     key = list(net._modules.keys())
     keylen = len(key)
     # for convbn
@@ -71,44 +70,50 @@ def fold_the_network(net):
                                              net._modules[key[flag + 1]])
                     flag += 1
         flag += 1
-    # for shiftadderbn
+
+def fold_the_addernet(net):
+    key = list(net._modules.keys())
+    keylen = len(key)
     flag = 0
     while flag < keylen:
-        if list(net._modules[key[flag]]._modules.keys()) != []:
-            fold_the_network(net._modules[key[flag]])
-        else:
-            if isinstance(net._modules[key[flag]],
-                          nn.Sequential) and flag + 1 < keylen:
-                if isinstance(net._modules[key[flag]][0],
-                              SEConv2d) and isinstance(
-                                  net._modules[key[flag]][1], Adder2d):
-                    if isinstance(net._modules[key[flag + 1]],
-                                  nn.BatchNorm2d) and flag + 2 < keylen:
-                        if isinstance(net._modules[key[flag + 2]],
-                                      nn.ReLU) or isinstance(
-                                          net._modules[key[flag + 2]],
-                                          nn.ReLU6):
-                            net._modules[key[flag]], net._modules[key[
-                                flag +
+        # if list(net._modules[key[flag]]._modules.keys()) != []:
+        fold_the_network(net._modules[key[flag]])
+        # else:
+        if isinstance(net._modules[key[flag]],
+                      nn.Sequential) and flag + 1 < keylen:
+            if isinstance(net._modules[key[flag]][0], SEConv2d) and isinstance(
+                    net._modules[key[flag]][1], Adder2d):
+                if isinstance(net._modules[key[flag + 1]],
+                              nn.BatchNorm2d) and flag + 2 < keylen:
+                    if isinstance(net._modules[key[flag + 2]],
+                                  nn.ReLU) or isinstance(
+                                      net._modules[key[flag + 2]], nn.ReLU6):
+                        net._modules[key[flag]], net._modules[
+                            key[flag +
                                 1]], net._modules[key[flag + 2]] = fold_SABR(
                                     net._modules[key[flag]],
                                     net._modules[key[flag + 1]],
                                     net._modules[key[flag + 2]])
-                            flag += 2
-                        else:
-                            net._modules[key[flag]], net._modules[key[
-                                flag + 1]] = fold_SAB(
-                                    net._modules[key[flag]],
-                                    net._modules[key[flag + 1]])
-                            flag += 1
-                    elif isinstance(net._modules[key[flag + 1]],
-                                    nn.BatchNorm2d):
+                        flag += 2
+                    else:
                         net._modules[key[flag]], net._modules[key[
                             flag + 1]] = fold_SAB(net._modules[key[flag]],
-                                                 net._modules[key[flag + 1]])
+                                                  net._modules[key[flag + 1]])
                         flag += 1
+                elif isinstance(net._modules[key[flag + 1]], nn.BatchNorm2d):
+                    net._modules[key[flag]], net._modules[key[
+                        flag + 1]] = fold_SAB(net._modules[key[flag]],
+                                              net._modules[key[flag + 1]])
+                    flag += 1
         flag += 1
 
+def fold_the_network(net, fold_type='conv'):
+    if fold_type == 'conv':
+        fold_the_convnet(net) 
+    elif fold_type == 'add':
+        fold_the_addernet(net)
+    else:
+        raise NotImplementedError()
 
 def make_the_shortcut_share(net, show=False):
     keys = list(net._modules.keys())
